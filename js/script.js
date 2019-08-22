1857713a3b4222525444be38d558ac8d14ee192c
gyrii84@gmail.com <gyrii84@gmail.com>
2019-08-22 22:55:46 +0300

    add % belowe progressbar in js. version 1.0

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function userProgressMoney(currentValue, maxValue){
 }
 
 var maxValue = 1200000;
-var currentValue = 100000;
+var currentValue = 550000;
 
 var res = userProgressMoney(currentValue, maxValue);
 
@@ -13,7 +13,7 @@ var progressBar = document.querySelector('#user-progress');
 progressBar.value = res.toFixed(1);
 
 var blockCurrentSum = document.querySelector('.blockCurrentSum');
-blockCurrentSum.innerHTML = 'Вже зібрали: ' + currentValue + ' грн.';
+blockCurrentSum.innerHTML = 'Вже зібрали: ' + currentValue + ' грн. або ' + progressBar.value + ' %';
 
 var blockMaxSum = document.querySelector('.blockMaxSum');
 blockMaxSum.innerHTML ='Наша мета - зібрати ' + maxValue + ' грн.';
